f9654e7ee393b280db345057b471ba5c88bbcc35
matt-henri <mathmarkub@gmail.com>
2023-05-15 17:48:00 -0300

    atualizado

--- ReajusteSalarial/src/entities/Reajuste.java
+++ ReajusteSalarial/src/entities/Reajuste.java
@@ -1,9 +1,33 @@
 package entities;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import application.Salario;
+
 public class Reajuste {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Salario emp = new Salario();
+		
+		System.out.print("Nome: ");
+		emp.nome = sc.nextLine();
+		System.out.print("Salário Bruto: ");
+		emp.salarioBruto = sc.nextDouble();
+		System.out.print("Desconto: ");
+		emp.desconto = sc.nextDouble();
+		
+		
+		
+		
+		
+		
+		
+		sc.close();
 
 	}
 
